90790a470272db2e2e94dbba11b24b01f60c4948
ParkSeungHa <tmdgk1031@gmail.com>
2020-07-13 15:18:58 +0900

    출/퇴근관리(Daily) 페이징 업데이트

--- src/main/java/com/dreamcapsule/project/apps/daily/service/DailyService.java
+++ src/main/java/com/dreamcapsule/project/apps/daily/service/DailyService.java
@@ -1,5 +1,6 @@
 package com.dreamcapsule.project.apps.daily.service;
 
+import com.dreamcapsule.project.domain.Criteria;
 import com.dreamcapsule.project.domain.DailyDomain;
 
 import java.util.List;
@@ -7,6 +8,10 @@ import java.util.List;
 public interface DailyService {
 
     public List<DailyDomain> findAll();
+
+    public List<DailyDomain> listPage(Criteria cri) throws Exception;
+    public int listCount() throws Exception;
+
 
 
 }
